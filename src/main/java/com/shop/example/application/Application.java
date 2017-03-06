3e4dd808ec1ecebb0d8f3c303958694e8f568326
simo <clouds.simo@gmail.com>
2017-03-06 11:45:59 +0000

    Added Instructions in readme file

--- src/main/java/com/shop/example/application/Application.java
+++ src/main/java/com/shop/example/application/Application.java
@@ -1,18 +1,25 @@
 package com.shop.example.application;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EvictionPolicy;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.HazelcastInstance;
 import com.shop.example.configuration.Properties;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.YamlJavaBeanPropertyConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication(scanBasePackages = "com.shop.example")
 @EnableConfigurationProperties(Properties.class)
+//@EnableCaching
 public class Application {
 
 
@@ -24,4 +31,24 @@ public class Application {
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
+
+
+//    @Bean
+//    public Config config() {
+//        return new Config().addMapConfig(
+//                new MapConfig()
+//                        .setName("accepted-messages")
+//                        .setEvictionPolicy(EvictionPolicy.LRU)
+//                        .setTimeToLiveSeconds(2400));
+//    }
+
+
+/*    @Bean
+    public ClientConfig client(){
+        ClientConfig cc = new ClientConfig();
+        cc.getNetworkConfig().addAddress("172.17.0.1:5701");
+        return cc;
+
+    }
+    */
 }
--- src/main/java/com/shop/example/service/MovieServiceImpl.java
+++ src/main/java/com/shop/example/service/MovieServiceImpl.java
@@ -1,7 +1,11 @@
 package com.shop.example.service;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.shop.example.configuration.Properties;
 import com.shop.example.dto.FilmDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,14 +13,30 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class MovieServiceImpl implements MovieService{
 
+    private Logger log = LoggerFactory.getLogger(MovieServiceImpl.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
     @Autowired
     private Properties properties;
 
+//    @Autowired
+//    private HazelcastInstance client;
+
     @Override
     public FilmDetails getFilmDetailsByTitle(String title) {
-        return restTemplate.getForObject(properties.getMovies().getMovieDetailsEndpoint() + title, FilmDetails.class);
+
+        FilmDetails fm = restTemplate.getForObject(properties.getMovies().getMovieDetailsEndpoint() + title, FilmDetails.class);
+
+//        IMap map = client.getMap("movie");
+//
+//        if(map != null){
+//            log.info("id " + map.get("id"));
+//            log.info("Language " + map.get("Language"));
+//        }
+
+
+        return fm;
     }
 }
